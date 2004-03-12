8d6e5385eb924bcff6c79f38c89fb973159ae39a
Peter S. Housel <housel@acm.org>
2004-03-12 21:13:27 +0000

    Don't try to use fdce for builds, it doesn't accept the options we give it.

--- admin/builds/find-compiler.bat
+++ admin/builds/find-compiler.bat
@@ -27,7 +27,7 @@ set DYLAN_RELEASE_COMPILER=%DYLAN_RELEASE_ROOT%\bin\minimal-console-compiler.exe
 if exist "%DYLAN_RELEASE_COMPILER%" goto compiler_found
 set DYLAN_RELEASE_COMPILER=%DYLAN_RELEASE_ROOT%\bin\basic-console-compiler.exe
 if exist "%DYLAN_RELEASE_COMPILER%" goto compiler_found
-set DYLAN_RELEASE_COMPILER=%DYLAN_RELEASE_ROOT%\bin\fdce.exe
+set DYLAN_RELEASE_COMPILER=%DYLAN_RELEASE_ROOT%\bin\fdbc.exe
 if exist "%DYLAN_RELEASE_COMPILER%" goto compiler_found
 set DYLAN_RELEASE_COMPILER=%DYLAN_RELEASE_ROOT%\bin\console-environment.exe
 if exist "%DYLAN_RELEASE_COMPILER%" goto compiler_found
