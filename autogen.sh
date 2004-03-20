6137f981c942c5961902d8a61ccbf48a993871ff
Peter S. Housel <housel@acm.org>
2004-03-20 23:26:42 +0000

    Add an autogen.sh to simplify building from CVS a little bit.

---
+++ autogen.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+DIE=false
+
+(autoconf --version && autoheader --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have autoconf installed to compile Functional Developer."
+    echo "Download the appropriate package for your distribution,"
+    echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/autoconf"
+    DIE=true
+}
+
+(aclocal --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have automake installed to compile Functional Developer."
+    echo "Download the appropriate package for your distribution,"
+    echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/automake"
+    DIE=true
+}
+
+(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have libtool installed to compile Functional Developer."
+    echo "Download the appropriate package for your distribution,"
+    echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/libtool"
+    DIE=true
+}
+
+$DIE && exit 1
+
+echo "processing (ignore errors and warnings from automake and libtoolize)..."
+
+( cd $srcdir
+  libtoolize --force --copy
+  automake --foreign --add-missing
+  autoconf )
