e7b129bc724a050ddd74672134442c40ff38164e
Hannes Mehnert <hannes@mehnert.org>
2010-03-27 01:18:28 +0000

    Bug 7396: remove some more emulator stuff

--- sources/qa/admin/builds/build-application.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-echo Building %1%.exe
-pushd %WEBSTER_PERSONAL_BUILD%\%1%
-build install-app
-popd
--- sources/qa/admin/builds/build-library.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-echo Building %1%.dll
-pushd %WEBSTER_PERSONAL_BUILD%\%1%
-build dll
-popd
