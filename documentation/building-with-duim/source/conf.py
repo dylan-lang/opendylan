4897614430c2e616c89c8dde5516fb712d22e7d9
Bruce Mitchener <bruce.mitchener@gmail.com>
2011-11-07 14:36:16 +0700

    Fix HTML titles of these documents.

--- documentation/building-with-duim/source/conf.py
+++ documentation/building-with-duim/source/conf.py
@@ -103,7 +103,7 @@ html_theme = 'agogo'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'Building Applications With DUIM'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
