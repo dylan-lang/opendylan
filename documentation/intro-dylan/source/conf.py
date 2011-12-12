62a27fd58eae950266b6f79dbbf06e654b00890c
Bruce Mitchener <bruce.mitchener@gmail.com>
2011-12-12 11:15:32 +0700

    Use the name Dylan Hackers.
    I'd used Dylan Maintainers here but didn't mean to.

--- documentation/intro-dylan/source/conf.py
+++ documentation/intro-dylan/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'An Introduction to Dylan'
-copyright = u'2011, Dylan Maintainers'
+copyright = u'2011, Dylan Hackers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -184,7 +184,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'IntroductiontoDylan.tex', u'An Introduction to Dylan',
-   u'Dylan Maintainers', 'manual'),
+   u'Dylan Hackers', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -214,7 +214,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'introductiontodylan', u'An Introduction to Dylan',
-     [u'Dylan Maintainers'], 1)
+     [u'Dylan Hackers'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -228,7 +228,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'IntroductiontoDylan', u'An Introduction to Dylan',
-   u'Dylan Maintainers', 'IntroductiontoDylan', 'One line description of project.',
+   u'Dylan Hackers', 'IntroductiontoDylan', 'One line description of project.',
    'Miscellaneous'),
 ]
 
@@ -246,9 +246,9 @@ texinfo_documents = [
 
 # Bibliographic Dublin Core info.
 epub_title = u'An Introduction to Dylan'
-epub_author = u'Dylan Maintainers'
-epub_publisher = u'Dylan Maintainers'
-epub_copyright = u'2011, Dylan Maintainers'
+epub_author = u'Dylan Hackers'
+epub_publisher = u'Dylan Hackers'
+epub_copyright = u'2011, Dylan Hackers'
 
 # The language of the text. It defaults to the language option
 # or en if the language is not set.
