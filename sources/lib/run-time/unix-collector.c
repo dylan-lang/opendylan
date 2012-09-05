99fb1ff61bec1768c2725aa51fe0a2015479b00c
Bruce Mitchener <bruce.mitchener@gmail.com>
2012-09-06 01:03:49 +0700

    Start using the various heap debugging utilities that were previously Windows only.

--- sources/lib/run-time/unix-collector.c
+++ sources/lib/run-time/unix-collector.c
@@ -9,37 +9,3 @@
 
 #include "collector.c"
 
-
-/* Heap stat stuff */
-
-void clear_wrapper_stats ()
-{
-}
-
-
-void display_wrapper_stats ()
-{
-}
-
-void add_stat_for_object (void *object, void* wrapper, int size)
-{
-}
-
-typedef struct wrapper_stats_s *wrapper_stats_t;
-
-typedef struct wrapper_stats_s {
-  void *wrapper_address;
-  int  usage_count;
-  int  usage_size;
-} wrapper_stats_s;
-
-void display_stats_for_memory_usage ()
-{
-}
-
-void display_wrapper_breakpoints()
-{
-}
-
-#include "break.c"
-
