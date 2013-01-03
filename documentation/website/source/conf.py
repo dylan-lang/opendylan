e2cd6091f8392b060279f50c5bfccb16946f0ff9
Bruce Mitchener <bruce.mitchener@gmail.com>
2013-01-03 14:05:55 +0700

    Bump copyright date.

--- documentation/website/source/conf.py
+++ documentation/website/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Open Dylan'
-copyright = u'2011, Dylan Hackers'
+copyright = u'2011-2013, Dylan Hackers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
