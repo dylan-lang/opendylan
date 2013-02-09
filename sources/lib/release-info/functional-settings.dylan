dylan-lang/opendylan

commit d428759ee928aa0e4ea96eef177c3f331ba646c2

Rework settings - remove unneeded definitions

sources/lib/release-info/functional-settings.dylan
@@ -13,94 +13,16 @@ Warranty:     Distributed WITHOUT WARRANTY OF ANY KIND
 // the Open Dylan compiler.  Maybe they should be called something
 // like <dylan-hackers-settings>? --tc
 
-define settings <general-open-dylan-local-settings>
+define settings <open-dylan-local-settings>
     (<local-software-settings>)
-  key-name "Open Dylan";
-end settings <general-open-dylan-local-settings>;
-
-define settings <unversioned-open-dylan-local-settings>
-    (<general-open-dylan-local-settings>)
-  key-name "Open Dylan";
-end settings <unversioned-open-dylan-local-settings>;
-
-define settings <open-dylan-local-settings-1-0>
-    (<unversioned-open-dylan-local-settings>)
   key-name "1.0";
   slot library-packs :: <machine-word> = as(<machine-word>, 0);
-end settings <open-dylan-local-settings-1-0>;
+end settings <open-dylan-local-settings>;
 
-define settings <open-dylan-local-settings-1-1>
-    (<unversioned-open-dylan-local-settings>)
-  key-name "1.1";
-  slot service-pack :: <integer> = 0;
-  slot library-packs :: <machine-word> = as(<machine-word>, 0);
-  slot console-tools :: <boolean> = #f;
-end settings <open-dylan-local-settings-1-1>;
-
-define settings <open-dylan-local-settings-1-2>
-    (<unversioned-open-dylan-local-settings>)
-  key-name "1.2";
-  slot service-pack :: <integer> = 0;
-  slot library-packs :: <machine-word> = as(<machine-word>, 0);
-  slot console-tools :: <boolean> = #f;
-end settings <open-dylan-local-settings-1-2>;
-
-define settings <open-dylan-local-settings-2-0>
-    (<unversioned-open-dylan-local-settings>)
-  key-name "2.0";
-  slot service-pack :: <integer> = 0;
-  slot library-packs :: <machine-word> = as(<machine-word>, 0);
-  slot console-tools :: <boolean> = #f;
-end settings <open-dylan-local-settings-2-0>;
-
-define settings <open-dylan-local-settings-2-1>
-    (<unversioned-open-dylan-local-settings>)
-  key-name "2.1";
-  slot service-pack :: <integer> = 0;
-  slot library-packs :: <machine-word> = as(<machine-word>, 0);
-  slot console-tools :: <boolean> = #f;
-end settings <open-dylan-local-settings-2-1>;
-
-define constant <open-dylan-local-settings>
-  = <open-dylan-local-settings-1-0>;
-
-define settings <general-open-dylan-user-settings>
+define settings <open-dylan-user-settings>
     (<current-user-software-settings>)
-  key-name "Open Dylan";
-end settings <general-open-dylan-user-settings>;
-
-define settings <unversioned-open-dylan-user-settings>
-    (<general-open-dylan-user-settings>)
-  key-name "Open Dylan";
-end settings <unversioned-open-dylan-user-settings>;
-
-define settings <open-dylan-user-settings-1-0>
-    (<unversioned-open-dylan-user-settings>)
   key-name "1.0";
-end settings <open-dylan-user-settings-1-0>;
-
-define settings <open-dylan-user-settings-1-1>
-    (<unversioned-open-dylan-user-settings>)
-  key-name "1.1";
-end settings <open-dylan-user-settings-1-1>;
-
-define settings <open-dylan-user-settings-1-2>
-    (<unversioned-open-dylan-user-settings>)
-  key-name "1.2";
-end settings <open-dylan-user-settings-1-2>;
-
-define settings <open-dylan-user-settings-2-0>
-    (<unversioned-open-dylan-user-settings>)
-  key-name "2.0";
-end settings <open-dylan-user-settings-2-0>;
-
-define settings <open-dylan-user-settings-2-1>
-    (<unversioned-open-dylan-user-settings>)
-  key-name "2.1";
-end settings <open-dylan-user-settings-2-1>;
-
-define constant <open-dylan-user-settings>
-  = <open-dylan-user-settings-1-0>;
+end settings <open-dylan-user-settings>;
 
 
 /// Library and service pack settings

sources/lib/release-info/module.dylan
@@ -135,20 +135,6 @@ define module release-info
   */
 
   // Settings
-  export <general-open-dylan-local-settings>,
-	 <general-open-dylan-user-settings>,
-	 <unversioned-open-dylan-local-settings>,
-         <unversioned-open-dylan-user-settings>,
-         <open-dylan-local-settings-1-0>,
-         <open-dylan-local-settings-1-1>,
-         <open-dylan-local-settings-1-2>,
-         <open-dylan-local-settings-2-0>,
-         <open-dylan-local-settings-2-1>,
-	 <open-dylan-local-settings>,
-	 <open-dylan-user-settings-1-0>,
-	 <open-dylan-user-settings-1-1>,
-	 <open-dylan-user-settings-1-2>,
-	 <open-dylan-user-settings-2-0>,
-	 <open-dylan-user-settings-2-1>,
+  export <open-dylan-local-settings>,
 	 <open-dylan-user-settings>;
 end module release-info;

commit e2c21dc7bca0242185e8e15e2ccd90602a7b1c93

[Windows] Fix location of registry settings.
This was broken in commit d428759ee928aa0e4ea96eef177c3f331ba646c2
resulting in the installer setting some settings that the IDE was
then not reading.

sources/lib/release-info/functional-settings.dylan
@@ -13,14 +13,34 @@ Warranty:     Distributed WITHOUT WARRANTY OF ANY KIND
 // the Open Dylan compiler.  Maybe they should be called something
 // like <dylan-hackers-settings>? --tc
 
-define settings <open-dylan-local-settings>
+define settings <general-open-dylan-local-settings>
     (<local-software-settings>)
+  key-name "Open Dylan";
+end settings <general-open-dylan-local-settings>;
+
+define settings <unversioned-open-dylan-local-settings>
+    (<general-open-dylan-local-settings>)
+  key-name "Open Dylan";
+end settings <unversioned-open-dylan-local-settings>;
+
+define settings <open-dylan-local-settings>
+    (<unversioned-open-dylan-local-settings>)
   key-name "1.0";
   slot library-packs :: <machine-word> = as(<machine-word>, 0);
 end settings <open-dylan-local-settings>;
 
-define settings <open-dylan-user-settings>
+define settings <general-open-dylan-user-settings>
     (<current-user-software-settings>)
+  key-name "Open Dylan";
+end settings <general-open-dylan-user-settings>;
+
+define settings <unversioned-open-dylan-user-settings>
+    (<general-open-dylan-user-settings>)
+  key-name "Open Dylan";
+end settings <unversioned-open-dylan-user-settings>;
+
+define settings <open-dylan-user-settings>
+    (<unversioned-open-dylan-user-settings>)
   key-name "1.0";
 end settings <open-dylan-user-settings>;
 