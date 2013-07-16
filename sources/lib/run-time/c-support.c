f2f685685fc2c06265571f251ca8427b31a328e5
Bruce Mitchener <bruce.mitchener@gmail.com>
2013-07-16 10:11:21 +0700

    [rt] Stub out symbols for the C run-time.

---
+++ sources/lib/run-time/c-support.c
@@ -0,0 +1,27 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+void *call_dylan_function(void *function, size_t arg_count, ...)
+{
+}
+
+void *dylan_keyboard_break_handler = NULL;
+
+bool dylan_keyboard_interruptQ = false;
+bool Prunning_under_dylan_debuggerQ = false;
+
+void* get_current_thread_handle()
+{
+  return NULL;
+}
+
+void *class_allocation_break(char *string, void *class, int count, int size)
+{
+  return NULL;
+}
+
+void *dylan_callin_internal(void *arg_base, size_t s)
+{
+  return NULL;
+}
+
