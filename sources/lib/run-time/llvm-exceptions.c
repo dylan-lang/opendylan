9a58f3b5f4dcec128d2ccb3020cc56d9adace287
Peter S. Housel <housel@acm.org>
2015-03-12 19:10:28 -0700

    Bug 7458: Trap arithmetic exceptions on Mac OS X
    * sources/lib/run-time/llvm-exceptions.c
      (exception_port): New static variable.
      (catcher): New thread function for running mach_exc_server.
      (catch_mach_exception_raise, catch_mach_exception_raise_state): New
       dummy handler functions called by mach_exc_server.
      (catch_mach_exception_raise_state_identity): New Mach exception handler
       function for EXC_ARITHMETIC exceptions.
      (EstablishDylanExceptionHandlers): Implement by starting a server
       thread when needed and setting the current thread's exception port
       for EXC_ARITHMETIC to exception_port.
      (RemoveDylanExceptionHandlers): Edit commentary.
    
    * sources/lib/run-time/Makefile.in: Add rules for running MIG (the
      Mach Interface Generator) to generate server interface codewhen
      building for Mac OS X. Build the generated mach_excServer.c source
      file into the LLVM run-time.

--- sources/lib/run-time/llvm-exceptions.c
+++ sources/lib/run-time/llvm-exceptions.c
@@ -148,14 +148,185 @@ void RemoveDylanExceptionHandlers(void)
 
 #elif defined OPEN_DYLAN_PLATFORM_DARWIN
 
+// References:
+// - http://www.mikeash.com/pyblog/friday-qa-2013-01-11-mach-exception-handlers.html
+// - Memory Pool System sources, code/protxc.c
+
+#include <stdio.h>
+#include <stddef.h>
+#include <pthread.h>
+
+#include <mach/mach.h>
+
+#include "mach_exc.h"
+
+#if defined OPEN_DYLAN_ARCH_X86_64
+#define THREAD_STATE_FLAVOR x86_THREAD_STATE64
+#elif defined OPEN_DYLAN_ARCH_X86
+#define THREAD_STATE_FLAVOR x86_THREAD_STATE32
+#endif
+
+#define STACK_ALIGNMENT 16
+
+static mach_port_t exception_port = MACH_PORT_NULL;
+
+void *catcher(void *dummy)
+{
+  extern boolean_t mach_exc_server(mach_msg_header_t *InHeadP,
+                                   mach_msg_header_t *OutHeadP);
+
+  pthread_setname_np("Dylan arithmetic exception catcher");
+  mach_msg_server(mach_exc_server, 2048, exception_port, 0);
+  abort();
+}
+
+// We won't receive this since we're not asking for it
+kern_return_t catch_mach_exception_raise
+    (mach_port_t exception_port,
+     mach_port_t thread,
+     mach_port_t task,
+     exception_type_t exception,
+     mach_exception_data_t code,
+     mach_msg_type_number_t codeCnt)
+{
+  return KERN_FAILURE;
+}
+
+// We won't receive this since we're not asking for it
+kern_return_t catch_mach_exception_raise_state
+    (mach_port_t exception_port,
+     exception_type_t exception,
+     const mach_exception_data_t code,
+     mach_msg_type_number_t codeCnt,
+     int *flavor,
+     const thread_state_t old_state,
+     mach_msg_type_number_t old_stateCnt,
+     thread_state_t new_state,
+     mach_msg_type_number_t *new_stateCnt)
+{
+  return KERN_FAILURE;
+}
+
+kern_return_t catch_mach_exception_raise_state_identity
+    (mach_port_t exception_port,
+     mach_port_t thread,
+     mach_port_t task,
+     exception_type_t exception,
+     mach_exception_data_t code,
+     mach_msg_type_number_t codeCnt,
+     int *flavor,
+     thread_state_t old_state,
+     mach_msg_type_number_t old_stateCnt,
+     thread_state_t new_state,
+     mach_msg_type_number_t *new_stateCnt)
+{
+  uintptr_t handler;
+  switch (code[0]) {
+  case EXC_I386_DIV:
+    handler = (uintptr_t) &dylan_integer_divide_by_0_error;
+    break;
+
+  case EXC_I386_INTO:
+    handler = (uintptr_t) &dylan_integer_overflow_error;
+    break;
+
+  case EXC_I386_SSEEXTERR:
+    // code[1] contains the floating point status word
+    if (code[1] & FE_DIVBYZERO) {
+      handler = (uintptr_t) &dylan_float_divide_by_0_error;
+    }
+    else if (code[1] & FE_OVERFLOW) {
+      handler = (uintptr_t) &dylan_float_overflow_error;
+    }
+    else if (code[1] & FE_UNDERFLOW) {
+      handler = (uintptr_t) &dylan_float_underflow_error;
+    }
+    else {
+      fprintf(stderr, "Unhandled EXC_ARITHMETIC: code=%lld/%lld\n", 
+              code[0], code[1]);
+      abort();
+    }
+    break;
+
+  default:
+    fprintf(stderr, "Unhandled EXC_ARITHMETIC: code=%lld/%lld\n", 
+            code[0], code[1]);
+    abort();
+  }
+
+#if defined OPEN_DYLAN_ARCH_X86
+  x86_thread_state32_t *ts = (x86_thread_state32_t *) old_state;
+  uintptr_t *esp = (uintptr_t *) (ts->__esp & -STACK_ALIGNMENT);
+  *--esp = ts->__eip;
+  ts->__esp = (uintptr_t) esp;
+  ts->__eip = handler;
+#elif defined OPEN_DYLAN_ARCH_X86_64
+  x86_thread_state64_t *ts = (x86_thread_state64_t *) old_state;
+  uintptr_t *rsp = (uintptr_t *) (ts->__rsp & -STACK_ALIGNMENT);
+  *--rsp = ts->__rip;
+  ts->__rsp = (uintptr_t) rsp;
+  ts->__rip = handler;
+#else
+#error No definition for catch_mach_exception_raise_state_identity provided
+#endif
+  *new_stateCnt = old_stateCnt;
+  memcpy(new_state, old_state, old_stateCnt * sizeof(natural_t));
+
+  return KERN_SUCCESS;
+}
+
 void EstablishDylanExceptionHandlers(void)
 {
-  // FIXME
+  if (exception_port == MACH_PORT_NULL) {
+    // Need a port we can receive exceptions on
+    kern_return_t rc
+      = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_RECEIVE,
+                           &exception_port);
+    if (rc != KERN_SUCCESS) {
+      mach_error("mach_port_allocate send", rc);
+      abort();
+    }
+
+    // Need to be able to send on it too
+    rc = mach_port_insert_right(mach_task_self(), exception_port, exception_port,
+                                MACH_MSG_TYPE_MAKE_SEND);
+    if (rc != KERN_SUCCESS) {
+      mach_error("mach_port_insert_right", rc);
+      abort();
+    }
+
+    // Spawn a thread to serve exception requests
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+    pthread_t thread;
+    int prc = pthread_create(&thread, &attr, catcher, NULL);
+    pthread_attr_destroy(&attr);
+    if (prc != 0) {
+      fprintf(stderr, "%s: pthread_create returned %d\n",
+              __func__, prc);
+      abort();
+    }
+  }
+
+  // Set this thread's exception port
+  kern_return_t rc
+    = thread_set_exception_ports(mach_thread_self(),
+                                 EXC_MASK_ARITHMETIC,
+                                 exception_port,
+                                 EXCEPTION_STATE_IDENTITY|MACH_EXCEPTION_CODES,
+                                 THREAD_STATE_FLAVOR);
+  if (rc != KERN_SUCCESS) {
+    mach_error("thread_set_exception_ports", rc);
+    abort();
+  }
+
+  primitive_reset_float_environment();
 }
 
 void RemoveDylanExceptionHandlers(void)
 {
-  // FIXME
+  // Nothing to do (thread is about to exit)
 }
 
 #endif
