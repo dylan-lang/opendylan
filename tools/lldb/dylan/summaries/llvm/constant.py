a593d35d477dd30859dc14eeb05a90d72d7ad7e5
Bruce Mitchener <bruce.mitchener@gmail.com>
2015-05-03 23:56:44 +0700

    [lldb] Simplify LLVM constant summaries.
    Extra functions are no longer required now that summaries return
    the simplified display.

--- tools/lldb/dylan/summaries/llvm/constant.py
+++ tools/lldb/dylan/summaries/llvm/constant.py
@@ -1,22 +1,16 @@
 from dylan.accessors import *
 from dylan import summaries
 
-def llvm_float_constant_value(value):
+@summaries.register('<llvm-float-constant>', 'llvm', 'llvm')
+def llvm_float_constant_summary(value, internal_dict):
   type_value = dylan_slot_element_by_name(value, 'llvm-value-type')
-  type_name = summaries.dylan_object_summary(type_value, {})
+  type_name = summaries.summary(type_value)
   float_value = dylan_slot_element_by_name(value, 'llvm-float-constant-float')
   return '%s %s' % (type_name, dylan_float_data(float_value))
 
-@summaries.register('<llvm-float-constant>', 'llvm', 'llvm')
-def llvm_float_constant_summary(value, internal_dict):
-  return llvm_float_constant_value(value)
-
-def llvm_integer_constant_value(value):
-  type_value = dylan_slot_element_by_name(value, 'llvm-value-type')
-  type_name = summaries.dylan_object_summary(type_value, {})
-  integer_value = dylan_slot_element_by_name(value, 'llvm-integer-constant-integer')
-  return '%s %s' % (type_name, dylan_integer_value(integer_value))
-
 @summaries.register('<llvm-integer-constant>', 'llvm', 'llvm')
 def llvm_integer_constant_summary(value, internal_dict):
-  return llvm_integer_constant_value(value)
+  type_value = dylan_slot_element_by_name(value, 'llvm-value-type')
+  type_name = summaries.summary(type_value)
+  integer_value = dylan_slot_element_by_name(value, 'llvm-integer-constant-integer')
+  return '%s %s' % (type_name, dylan_integer_value(integer_value))
