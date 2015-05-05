6b59081a029b33fa72644db319394cafd96f7948
Bruce Mitchener <bruce.mitchener@gmail.com>
2015-05-05 21:58:31 +0700

    [lldb] Fix <llvm-argument> summary.
    * tools/lldb/dylan/summaries/llvm/function.py
      (llvm_argument_summary): Get summary of correct value.

--- tools/lldb/dylan/summaries/llvm/function.py
+++ tools/lldb/dylan/summaries/llvm/function.py
@@ -9,4 +9,4 @@ def llvm_argument_summary(value, internal_dict):
   # argument_name_value is false-or(<string>)
   if not dylan_is_boolean(argument_name_value):
     argument_name = ' ' + dylan_byte_string_data(argument_name_value)
-  return '%s%s' % (summaries.summary(value), argument_name)
+  return '%s%s' % (summaries.summary(argument_type), argument_name)
