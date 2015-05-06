957571798d2d4e896a0024e3c4b107468d2d76aa
Bruce Mitchener <bruce.mitchener@gmail.com>
2015-05-06 20:37:16 +0700

    [lldb] Add <llvm-metadata-string> summary.
    * tools/lldb/dylan/summaries/llvm/misc.py
      (llvm_metadata_string_summary): New.

--- tools/lldb/dylan/summaries/llvm/misc.py
+++ tools/lldb/dylan/summaries/llvm/misc.py
@@ -9,6 +9,11 @@ def llvm_basic_block_summary(value, internal_dict):
     return ''
   return dylan_string_data(bblock_name_value)
 
+@summaries.register('<llvm-metadata-string>', 'llvm', 'llvm')
+def llvm_metadata_string_summary(value, internal_dict):
+  string_value = dylan_slot_element_by_name(value, 'llvm-metadata-string')
+  return dylan_string_data(string_value)
+
 @summaries.register('<llvm-named-metadata>', 'llvm', 'llvm')
 def llvm_named_metadata_summary(value, internal_dict):
   name_value = dylan_slot_element_by_name(value, 'llvm-named-metadata-name')
