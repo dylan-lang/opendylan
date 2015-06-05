decd65fadaec94741ba627778c2e3ccccc50edb3
Bruce Mitchener <bruce.mitchener@gmail.com>
2015-06-05 23:06:57 +0700

    [lldb] Add string & symbol fragment summaries.
    * tools/lldb/dylan/__init__.py: Add dfmc-reader summaries.
    
    * tools/lldb/dylan/summaries/dfmc/reader.py
      (string_fragment_summary, symbol_fragment_summary): New.

--- tools/lldb/dylan/summaries/dfmc/reader.py
+++ tools/lldb/dylan/summaries/dfmc/reader.py
@@ -11,3 +11,14 @@ from dylan import summaries
 def name_fragment_summary(value, internal_dict):
   name = dylan_slot_element_by_name(value, 'fragment-name')
   return dylan_symbol_name(name)
+
+@summaries.register('<string-fragment>', 'dfmc-reader', 'dfmc-reader')
+def string_fragment_summary(value, internal_dict):
+  value = dylan_slot_element_by_name(value, 'fragment-value')
+  return dylan_string_data(value)
+
+@summaries.register('<keyword-syntax-symbol-fragment>', 'dfmc-reader', 'dfmc-reader')
+@summaries.register('<symbol-syntax-symbol-fragment>', 'dfmc-reader', 'dfmc-reader')
+def symbol_fragment_summary(value, internal_dict):
+  value = dylan_slot_element_by_name(value, 'fragment-value')
+  return dylan_symbol_name(value)
