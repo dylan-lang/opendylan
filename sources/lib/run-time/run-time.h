1f6ecc6c45f951505816af9a758a00181b02827a
Kamil Rytarowski <n54@gmx.com>
2015-09-28 02:56:56 +0200

    alloca() inhabits <stdlib.h> in the BSD world

--- sources/lib/run-time/run-time.h
+++ sources/lib/run-time/run-time.h
@@ -1426,13 +1426,10 @@ void *primitive_alloc_rt(size_t size,
 
 #ifdef OPEN_DYLAN_PLATFORM_WINDOWS
 #  include <malloc.h>
+#elif defined(OPEN_DYLAN_PLATFORM_FREEBSD) || defined(OPEN_DYLAN_PLATFORM_NETBSD)
+#  include <stdlib.h>
 #else
-#  ifdef OPEN_DYLAN_PLATFORM_FREEBSD
-#     include <sys/types.h>
-      extern void * alloca (size_t size);
-#  else
-#    include <alloca.h>
-#  endif
+#  include <alloca.h>
 #endif
 
 #define primitive_stack_allocate(sz) ((dylan_value)(alloca((int)(sz) * sizeof(dylan_value))))
