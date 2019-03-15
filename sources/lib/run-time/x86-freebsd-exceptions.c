0bb6a295cf365923381d782be6b12e1ebc9ce0ef
Peter S. Housel <housel@acm.org>
2019-03-14 23:32:19 -0700

    run-time: Fix HARP back-end arithmetic traps on FreeBSD
    * sources/lib/run-time/x86-freebsd-exceptions.c
      (SetIP): New function, adapted from LLVM run-time.
      (RestoreFPState): Update to use fenv.h functions.
      (DylanFPEHandler): Set post-exception handler instruction pointer
       using SetIP()).

--- sources/lib/run-time/x86-freebsd-exceptions.c
+++ sources/lib/run-time/x86-freebsd-exceptions.c
@@ -16,11 +16,23 @@ extern void dylan_float_underflow_handler();
 /* ---*** TODO: Find out how to trap stack overflows and add an appropriate handler */
 
 #include <sys/signal.h>
+#include <sys/ucontext.h>
 #include <ucontext.h>
 #include <ieeefp.h>
 #include <fenv.h>
+#include <unistd.h>
 
 #include "stack-walker.h"
+
+static inline void SetIP(void *uap, uintptr_t ip)
+{
+  ucontext_t *uc = (ucontext_t *) uap;
+  // Simulate the effect of a calll instruction
+  uintptr_t *esp = (uintptr_t *) uc->uc_mcontext.mc_esp;
+  *--esp = uc->uc_mcontext.mc_eip;
+  uc->uc_mcontext.mc_esp = (uintptr_t) esp;
+  uc->uc_mcontext.mc_eip = ip;
+}
 
 #define EXCEPTION_PREAMBLE() \
   struct sigaction oldFPEHandler; \
@@ -38,8 +50,8 @@ static void DylanTRAPHandler (int sig, siginfo_t *info, void *sc);
 static __inline
 void RestoreFPState ()
 {
-  fpresetsticky(fpgetsticky());
-  fpsetmask(FP_X_INV | FP_X_DZ | FP_X_OFL | FP_X_UFL);
+  feclearexcept(FE_ALL_EXCEPT);
+  feenableexcept(FE_DIVBYZERO | FE_OVERFLOW | FE_UNDERFLOW | FE_INVALID);
 }
 
 static void EstablishDylanExceptionHandlers (struct sigaction * oldFPEHandler,
@@ -74,40 +86,35 @@ static void RemoveDylanExceptionHandlers (struct sigaction * oldFPEHandler,
 static void DylanFPEHandler (int sig, siginfo_t *info, void *uap)
 {
   if (inside_dylan_ffi_barrier() == 0) {
-  } else {
-    ucontext_t *uc = (ucontext_t *) uap;
-
+  }
+  else {
     switch (info->si_code) {
     case FPE_INTDIV:
-      RestoreFPState();
-      uc->uc_mcontext.mc_eip = (long) dylan_integer_divide_0_handler;
+      SetIP(uap, (uintptr_t) &dylan_integer_divide_0_handler);
       break;
 
     case FPE_INTOVF:
-      RestoreFPState();
-      uc->uc_mcontext.mc_eip = (long) dylan_integer_overflow_handler;
+      SetIP(uap, (uintptr_t) &dylan_integer_overflow_handler);
       break;
 
     case FPE_FLTDIV:
-      RestoreFPState();
-      uc->uc_mcontext.mc_eip = (long) dylan_float_divide_0_handler;
+      SetIP(uap, (uintptr_t) &dylan_float_divide_0_handler);
       break;
 
     case FPE_FLTINV:
-      RestoreFPState();
-      uc->uc_mcontext.mc_eip = (long) dylan_float_invalid_handler;
+      SetIP(uap, (uintptr_t) &dylan_float_invalid_handler);
+      break;
 
     case FPE_FLTOVF:
-      RestoreFPState();
-      uc->uc_mcontext.mc_eip = (long) dylan_float_overflow_handler;
+      SetIP(uap, (uintptr_t) &dylan_float_overflow_handler);
       break;
 
     case FPE_FLTUND:
-      RestoreFPState();
-      uc->uc_mcontext.mc_eip = (long) dylan_float_underflow_handler;
+      SetIP(uap, (uintptr_t) &dylan_float_underflow_handler);
       break;
 
     default:
+      abort();
       break;
     }
   }
