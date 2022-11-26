984e014006289d3c933cf5df33d0e0b535c1af4b
Peter S. Housel <housel@acm.org>
2022-11-25 20:32:49 -0800

    build: Update release-with-batteries for LLVM 15.x
    * build/unix/release-with-batteries.sh: Update to always build from
      LLVM sources, checked out from the upstream repository, and use
      the libunwind included with the selected LLVM version.

--- build/unix/release-with-batteries.sh
+++ build/unix/release-with-batteries.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 set -e
 
-LLVM_RELEASE=10.0.1
+LLVM_RELEASE=15.0.5
 LLVM_REL=$(echo $LLVM_RELEASE | sed s/-rc/rc/)
 
 LLVM_CLANG=$(echo $LLVM_RELEASE | sed 's/\([0-9]*\).*/\1/')
 
-LIBUNWIND_REV=22b615a96593f13109a27cabfd1764ec4f558c7a
-
-BDWGC_RELEASE=8.0.4
+BDWGC_RELEASE=8.2.2
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
@@ -34,11 +32,13 @@ USE_LLD="-fuse-ld=lld"
 BUILD_SRC=false
 case ${MACHINE}-${SYSTEM} in
     amd64-FreeBSD)
+        BUILD_SRC=:
         MAKE=gmake
         TRIPLE=amd64-unknown-freebsd11
         DYLAN_JOBS=$(getconf _NPROCESSORS_ONLN)
         ;;
     i386-FreeBSD)
+        BUILD_SRC=:
         MAKE=gmake
         TRIPLE=i386-unknown-freebsd11
         DYLAN_JOBS=$(getconf _NPROCESSORS_ONLN)
@@ -49,6 +49,7 @@ case ${MACHINE}-${SYSTEM} in
         DYLAN_JOBS=$(getconf _NPROCESSORS_ONLN)
         ;;
     aarch64-Linux)
+        BUILD_SRC=:
         TRIPLE=aarch64-linux-gnu
         TAR="tar --wildcards"
         DYLAN_JOBS=$(getconf _NPROCESSORS_ONLN)
@@ -62,139 +63,83 @@ case ${MACHINE}-${SYSTEM} in
         ;;
 esac
 
-LIBUNWIND_ZIP=${LIBUNWIND_REV}.zip
-
+LLVM_DIST=llvm-project-${LLVM_REL}
 BDWGC_DIST=gc-${BDWGC_RELEASE}
-BDWGC_TAR=${BDWGC_DIST}.tar.gz
 
-if $BUILD_SRC; then
-    LLVM_DIST=llvm-project-${LLVM_REL}
-    LLVM_TAR=${LLVM_DIST}.tar.xz
-    if [ ! -f "$LLVM_TAR" ]; then
-        echo Error: LLVM source distribution file missing 1>&2
-        echo Please download https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_RELEASE}/${LLVM_TAR} and place the file in the current directory 1>&2
-        exit 1
-    fi
-else
-    LLVM_DIST=clang+llvm-${LLVM_RELEASE}-${TRIPLE}
-    LLVM_TAR=${LLVM_DIST}.tar.xz
-    if [ ! -f "$LLVM_TAR" ]; then
-        echo Error: LLVM binary distribution file missing 1>&2
-        echo Please download https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_RELEASE}/${LLVM_TAR} and place the file in the current directory 1>&2
-        exit 1
-    fi
-fi
-if $NEED_LIBUNWIND && [ ! -f "$LIBUNWIND_ZIP" ]; then
-    echo Error: LLVM libunwind source distribution file missing 1>&2
-    echo Please download https://github.com/llvm/llvm-project/archive/${LIBUNWIND_ZIP} and place the file in the current directory 1>&2
-    exit 1
-fi
-if [ ! -f "$BDWGC_TAR" ]; then
-    echo Error: BDW GC distribution file missing 1>&2
-    echo Please download https://github.com/ivmai/bdwgc/releases/download/v${BDWGC_RELEASE}/${BDWGC_TAR} and place the file in the current directory 1>&2
-    exit 1
+if [ ! -d ${LLVM_DIST} ]; then
+    echo Cloning llvm-project tag ${LLVM_REL}
+    git clone --depth 1 --branch llvmorg-${LLVM_REL} https://github.com/llvm/llvm-project.git ${LLVM_DIST}
 fi
 
-if $BUILD_SRC; then
-    echo Unpacking LLVM project sources into ${LLVM_DIST}
-    rm -rf "${LLVM_DIST}"
-    ${TAR} -xJf ${LLVM_TAR} \
-           ${LLVM_DIST}/llvm \
-           ${LLVM_DIST}/clang \
-           ${LLVM_DIST}/lld
-    (cd ${LLVM_DIST};
-     cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=OFF \
-           -DLLVM_ENABLE_PROJECTS="llvm;clang;lld" \
-           -DLLVM_ENABLE_TERMINFO=OFF \
-           -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON \
-           -DLLVM_BUILD_TOOLS=OFF \
-           -DLLVM_BUILD_UTILS=OFF \
-           -DLLVM_TOOL_LLVM_ISEL_FUZZER_BUILD=OFF \
-           -DLLVM_TOOL_LLVM_OPT_FUZZER_BUILD=OFF \
-           -DLLVM_TOOL_LLVM_ITANIUM_DEMANGLE_FUZZER_BUILD=OFF \
-           -DLLVM_TOOL_LLVM_MICROSOFT_DEMANGLE_FUZZER_BUILD=OFF \
-           -DLLVM_TOOL_LLVM_SPECIAL_CASE_LIST_FUZZER_BUILD=OFF \
-           -DLLVM_TOOL_LLVM_YAML_NUMERIC_PARSER_FUZZER_BUILD=OFF \
-           -DLLVM_INCLUDE_TESTS=OFF \
-           -DCLANG_ENABLE_STATIC_ANALYZER=OFF \
-           -DCLANG_ENABLE_ARCMT=OFF \
-           -DCLANG_INCLUDE_DOCS=OFF \
-           -DCLANG_INCLUDE_TESTS=OFF \
-           -DCLANG_TOOL_ARCMT_TEST_BUILD=OFF \
-           -DCLANG_TOOL_CLANG_CHECK_BUILD=OFF \
-           -DCLANG_TOOL_CLANG_DIFF_BUILD=OFF \
-           -DCLANG_TOOL_CLANG_EXTDEF_MAPPING_BUILD=OFF \
-           -DCLANG_TOOL_CLANG_FORMAT_BUILD=OFF \
-           -DCLANG_TOOL_CLANG_FORMAT_VS_BUILD=OFF \
-           -DCLANG_TOOL_CLANG_FUZZER_BUILD=OFF \
-           -DCLANG_TOOL_CLANG_IMPORT_TEST_BUILD=OFF \
-           -DCLANG_TOOL_CLANG_OFFLOAD_BUNDLER_BUILD=OFF \
-           -DCLANG_TOOL_CLANG_OFFLOAD_WRAPPER_BUILD=OFF \
-           -DCLANG_TOOL_CLANG_REFACTOR_BUILD=OFF \
-           -DCLANG_TOOL_CLANG_RENAME_BUILD=OFF \
-           -DCLANG_TOOL_CLANG_SCAN_DEPS_BUILD=OFF \
-           -DCLANG_TOOL_CLANG_SHLIB_BUILD=OFF \
-           -DCLANG_TOOL_C_ARCMT_TEST_BUILD=OFF \
-           -DCLANG_TOOL_C_INDEX_TEST_BUILD=OFF \
-           -DCLANG_TOOL_DIAGTOOL_BUILD=OFF \
-           -DCLANG_TOOL_LIBCLANG_BUILD=OFF \
-           -DCLANG_TOOL_SCAN_BUILD_BUILD=OFF \
-           -DCLANG_TOOL_SCAN_VIEW_BUILD=OFF \
-           -DLLVM_PARALLEL_COMPILE_JOBS=${DYLAN_JOBS} \
-           llvm;
-     ninja)
-    mkdir -p ${DISTDIR}/bin ${DISTDIR}/lib
-    cp -RP \
-           ${LLVM_DIST}/bin/clang-[1-9]* \
-           ${LLVM_DIST}/bin/clang \
-           ${LLVM_DIST}/bin/clang++ \
-           ${LLVM_DIST}/bin/lld \
-           ${LLVM_DIST}/bin/ld.lld \
-           ${DISTDIR}/bin/
-    cp -RP ${LLVM_DIST}/lib/clang ${DISTDIR}/lib/
-else
-    echo Unpacking LLVM into ${LLVM_DIST}
-    rm -rf "${LLVM_DIST}"
-    ${TAR} -xJf ${LLVM_TAR} \
-           "${LLVM_DIST}/bin/clang-[1-9]*" \
-           ${LLVM_DIST}/bin/clang \
-           ${LLVM_DIST}/bin/clang++ \
-           ${LLVM_DIST}/bin/lld \
-           ${LLVM_DIST}/bin/ld.lld \
-           ${LLVM_DIST}/lib/clang
-    cp -RP ${LLVM_DIST}/* ${DISTDIR}/
+if [ ! -d ${BDWGC_DIST} ]; then
+    echo Cloning BDW GC tag ${BDWGC_RELEASE}
+    git clone --depth 1 --branch v${BDWGC_RELEASE} https://github.com/ivmai/bdwgc.git ${BDWGC_DIST}
 fi
+
+RT_OPTS=
+RT_TARGETS=
+if $NEED_LIBUNWIND; then
+    RT_OPTS="-DLLVM_ENABLE_RUNTIMES:STRING=libunwind -DLIBUNWIND_INSTALL_HEADERS:BOOL=ON"
+    RT_TARGETS=install-runtimes
+fi
+(cd ${LLVM_DIST};
+ cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=OFF \
+       -DCMAKE_INSTALL_PREFIX=${DISTDIR} \
+       -DLLVM_TARGETS_TO_BUILD:STRING="Native" \
+       -DLLVM_ENABLE_PROJECTS="llvm;clang;lld" \
+       ${RT_OPTS} \
+       -DLLVM_ENABLE_TERMINFO=OFF \
+       -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON \
+       -DLLVM_BUILD_TOOLS=OFF \
+       -DLLVM_BUILD_UTILS=OFF \
+       -DLLVM_TOOL_LLVM_ISEL_FUZZER_BUILD=OFF \
+       -DLLVM_TOOL_LLVM_OPT_FUZZER_BUILD=OFF \
+       -DLLVM_TOOL_LLVM_ITANIUM_DEMANGLE_FUZZER_BUILD=OFF \
+       -DLLVM_TOOL_LLVM_MICROSOFT_DEMANGLE_FUZZER_BUILD=OFF \
+       -DLLVM_TOOL_LLVM_SPECIAL_CASE_LIST_FUZZER_BUILD=OFF \
+       -DLLVM_TOOL_LLVM_YAML_NUMERIC_PARSER_FUZZER_BUILD=OFF \
+       -DLLVM_INCLUDE_TESTS=OFF \
+       -DCLANG_ENABLE_STATIC_ANALYZER=OFF \
+       -DCLANG_ENABLE_ARCMT=OFF \
+       -DCLANG_INCLUDE_DOCS=OFF \
+       -DCLANG_INCLUDE_TESTS=OFF \
+       -DCLANG_TOOL_ARCMT_TEST_BUILD=OFF \
+       -DCLANG_TOOL_CLANG_CHECK_BUILD=OFF \
+       -DCLANG_TOOL_CLANG_DIFF_BUILD=OFF \
+       -DCLANG_TOOL_CLANG_EXTDEF_MAPPING_BUILD=OFF \
+       -DCLANG_TOOL_CLANG_FORMAT_BUILD=OFF \
+       -DCLANG_TOOL_CLANG_FORMAT_VS_BUILD=OFF \
+       -DCLANG_TOOL_CLANG_FUZZER_BUILD=OFF \
+       -DCLANG_TOOL_CLANG_IMPORT_TEST_BUILD=OFF \
+       -DCLANG_TOOL_CLANG_OFFLOAD_BUNDLER_BUILD=OFF \
+       -DCLANG_TOOL_CLANG_OFFLOAD_WRAPPER_BUILD=OFF \
+       -DCLANG_TOOL_CLANG_REFACTOR_BUILD=OFF \
+       -DCLANG_TOOL_CLANG_RENAME_BUILD=OFF \
+       -DCLANG_TOOL_CLANG_SCAN_DEPS_BUILD=OFF \
+       -DCLANG_TOOL_CLANG_SHLIB_BUILD=OFF \
+       -DCLANG_TOOL_C_ARCMT_TEST_BUILD=OFF \
+       -DCLANG_TOOL_C_INDEX_TEST_BUILD=OFF \
+       -DCLANG_TOOL_DIAGTOOL_BUILD=OFF \
+       -DCLANG_TOOL_LIBCLANG_BUILD=OFF \
+       -DCLANG_TOOL_SCAN_BUILD_BUILD=OFF \
+       -DCLANG_TOOL_SCAN_VIEW_BUILD=OFF \
+       -DLLVM_PARALLEL_COMPILE_JOBS=${DYLAN_JOBS} \
+       llvm;
+ ninja install-clang install-lld install-clang-resource-headers ${RT_TARGETS})
 CC="${DISTDIR}/bin/clang${SYSROOT}"
 CXX="${DISTDIR}/bin/clang++${SYSROOT}"
 
 RTLIBS_INSTALL=
 
 if $NEED_LIBUNWIND; then
-    LIBUNWIND_DIST=llvm-project-${LIBUNWIND_REV}/libunwind
-    echo Unpacking libunwind into ${LIBUNWIND_DIST}
-    rm -rf llvm-project-${LIBUNWIND_REV}
-    unzip -q ${LIBUNWIND_REV}.zip "${LIBUNWIND_DIST}/*"
-
-    (cd ${LIBUNWIND_DIST};
-     cmake -G Ninja -DCMAKE_BUILD_TYPE=Release \
-           -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX .;
-     ninja)
-
-    mkdir -p ${DISTDIR}/include
-    cp -RP ${LIBUNWIND_DIST}/lib/libunwind.so* ${DISTDIR}/lib
-    cp -RP ${LIBUNWIND_DIST}/include/*.h ${DISTDIR}/include
-
-    for i in ${DISTDIR}/lib/libunwind*; do
+    for i in ${DISTDIR}/lib/*/libunwind*; do
         RTLIBS_INSTALL="$RTLIBS_INSTALL $i"
     done
 fi
 
-echo Unpacking BDWGC into ${BDWGC_DIST}
-rm -rf "${BDWGC_DIST}"
-${TAR} -xzf ${BDWGC_TAR}
-
 echo Building BDWGC in ${BDWGC_DIST}
 (cd ${BDWGC_DIST};
+ ./autogen.sh;
  ./configure CC="$CC" -q --prefix=$DISTDIR \
              --disable-docs --disable-static \
              --enable-threads=posix \
