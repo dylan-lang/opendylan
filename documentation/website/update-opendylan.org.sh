6b6f72d2bf744e0bf9c2d4d18275a054cf26d5d2
Carl Gay <carlgay@gmail.com>
2023-07-12 05:11:19 +0000

    Rework update scripts

--- documentation/website/update-opendylan.org.sh
+++ documentation/website/update-opendylan.org.sh
@@ -5,13 +5,19 @@
 logfile=/var/log/update-opendylan.org.`date +%Y%m%d%H%M`.log
 exec > $logfile 2>&1
 
-dest_dir=/var/www/opendylan.org
-opendylan_dir=/root/deploy-opendylan.org/opendylan
-gendoc_exe=/root/deploy-opendylan.org/gendoc/_build/bin/gendoc
-
 exe_dir="$(realpath $(dirname $0))"
 
-${exe_dir}/update.sh "${dest_dir}" "${opendylan_dir}" "${gendoc_exe}"
+repo_dir=/root/deploy-opendylan.org
+gendoc_exe=${repo_dir}/gendoc/_build/bin/gendoc
+dest_dir=/var/www/opendylan.org
+
+# Update opendylan and website submodule first so we get any changes
+# to the update.sh script.
+cd ${repo_dir}/opendylan
+git pull --rebase origin master
+git submodule update --init --recursive
+
+${exe_dir}/update.sh "${dest_dir}" "${repo_dir}" "${gendoc_exe}"
 
 echo "Done updating opendylan.org"
 bzip2 $logfile
--- documentation/website/update.sh
+++ documentation/website/update.sh
@@ -1,53 +1,42 @@
 #!/bin/bash -xe
 
-# This script builds the opendylan.org website. A summary of what this script
-# actually does:
-#
-# $ git clone --recursive https://github.com/dylan-lang/opendylan
-# $ git clone --recursive https://github.com/dylan-lang/gendoc
-# $ cd gendoc
-# $ make docs
-# $ cp -rp output/package ../opendylan/documentation/website/source/
-# $ cd ../opendylan/documentation/website
-# $ make html
-# $ cp -rp build/html/* /var/www/opendylan.org
+# This script builds the opendylan.org website. It does a clean build
+# each time and copies files to ${dest_dir}, meaning that there could
+# be cruft left around in ${dest_dir}. Might want to fix that some day.
 
 # /var/www/opendylan.org/downloads is handled specially. New versions of Open
-# Dylan must be copied there by hand.
-
-# TODO:
-# * Fix latexpdf vs python3 issues. Disabled PDF for now.
-# * Link to GitHub releases for the download pages instead of maintaining
-#   downloads by hand.
-# * Build/install dylan-programming-book into ${dest_dir}/books/dpg.
-# * Build/install dylan-reference-manual into ${dest_dir}/books/drm.
+# Dylan should be copied there by hand, or ... we could stop doing that.
 
 if [[ $# -ne 3 ]]; then
-    echo "Usage: `basename $0` dest_dir opendylan_dir gendoc_exe"
-    echo "  dest_dir:      Directory in which to place the built website documents."
-    echo "                 Example: /var/www/opendylan.org"
-    echo "  opendylan_dir: The opendylan repo directory."
-    echo "  gendoc_exe:    Path to the gendoc executable."
+    echo "Usage: `basename $0` dest_dir repo_dir gendoc_exe"
+    echo "  dest_dir:    Directory in which to place the built website documents."
+    echo "               Example: /var/www/opendylan.org"
+    echo "  repo_dir:    Directory in which opendylan, drm, and dpg repos live."
+    echo "  gendoc_exe:  Path to the gendoc executable."
     exit 2
 fi
 
 dest_dir=$(realpath "$1")
-opendylan_dir=$(realpath "$2")
+repo_dir=$(realpath "$2")
 gendoc_exe=$(realpath "$3")
 
-# Generate package docs
+# Not sure if necessary yet...
+#mkdir -p ${dest_dir}/_plantuml
+
+echo "Generating package docs..."
 gendoc_work_dir=$(mktemp -d gendoc-XXXX) # git clones go here
-gendoc_output_dir=${opendylan_dir}/documentation/website/source
+gendoc_output_dir=${repo_dir}/opendylan/documentation/website/source
 cd ${gendoc_work_dir}
-rm -rf ${package_doc_dir}
+rm -rf ${gendoc_output_dir}/package
 ${gendoc_exe} -o ${gendoc_output_dir}
 
-# Build website
-cd ${opendylan_dir}/documentation/website
-git pull --rebase origin master
-git submodule update --recursive --init
+echo "Building website..."
+cd ${repo_dir}/opendylan/documentation/website
 make clean
 make html
-
-# Deploy
 rsync -avz build/html/ ${dest_dir}
+
+echo "Updating DRM files..."
+cd ${repo_dir}/dylan-reference-manual
+git pull --rebase origin master
+rsync -avz source/  /var/www/opendylan.org/books/drm
